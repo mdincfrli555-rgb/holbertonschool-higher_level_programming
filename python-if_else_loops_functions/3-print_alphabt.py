03e1dcc41dd28aa4b5deb59823dc862f5bad94fa
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-13 17:17:42 +0000

    a

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char_code in range(97, 123):
-    if char_code != 101 and char_code != 113:
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
 
-    print("{:c}.format(char_code), end="")
+        print("{}".format(chr(letter)), end="")
