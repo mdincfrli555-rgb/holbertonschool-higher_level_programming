20400af94fcf57db8a782f2b1689c69fb84ab866
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-14 20:55:29 +0000

    a

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(count)) 
+
     for i in range(1, count + 1):
         print("{}: {}".format(i, sys.argv[i]))
