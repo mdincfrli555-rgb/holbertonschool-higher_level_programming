36b40c8e9f561202cb0031aa53a27d76a7110ae3
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-14 14:26:08 +0000

    a

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,4 +2,5 @@
 def fizzbuzz():
     for i in range(1, 101):
         res = "Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0)
-        print("{}{}".format(res or i, " " if i < 100 else ""), end="")
+
+        print("{}".format(res or i), end=" ")
