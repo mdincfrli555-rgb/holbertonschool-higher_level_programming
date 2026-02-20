4778c5369706031708d85e824be55a42d9972189
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-20 19:12:53 +0000

    a

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def
-square_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix=[]):
     new matrix = []
     for row in matrix:
-        new_row = []
-        for x in row:
+       new_row = []
+       for x in row:
             new row.append(x**2)
         new_matrix.append(new_row)
     return new_matrix
