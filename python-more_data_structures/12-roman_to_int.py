2ff40c14baecc1df170f545c47f169b9408e7fdb
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-21 14:02:33 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,7 @@ def roman_to_int(roman_string):
 
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         res = 0
-        for i in
-    range(len(roman_string)):
+        for i in range(len(roman_string)):
             val = d.get(roman_string[i], 0)
             next_val = d.get(roman_string[i+1], 0) if i + 1 < len(roman_string) else 0
             res += -val if val < next_val else val
