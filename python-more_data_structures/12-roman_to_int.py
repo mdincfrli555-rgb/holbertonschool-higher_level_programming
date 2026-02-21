89c0565dad27a07af39be346596fe2c561088f49
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-21 18:00:16 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
-            return 0
+def roman_to_int(roman_strin):
+    if not isintance(roman_string, str):
+        return 0
 
-        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        res = 0
-        for i in range(len(roman_string)):
-            val = d.get(roman_string[i], 0)
-            nxt_val = d.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
-            res += -val if val < nxt_val else val
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    for i in range(len(roman_string)):
+        val = d.get(roman_string[i], 0)
+        if i +1 < len(roman_sting) and val < d.get(roman_string[i+1], 0):
+            res -= val
+        else:
+            res += val
         return res
