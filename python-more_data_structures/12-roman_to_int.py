b2bc7225a5430a0b125d042dc317df7fa2711ceb
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-21 14:52:14 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,6 @@ def roman_to_int(roman_string):
         res = 0
         for i in range(len(roman_string)):
             val = d.get(roman_string[i], 0)
-            next_val = d.get(roman_string[i+1], 0)
-                                                  if i + 1 < len(roman_string) else 0
-            res += -val if val < next_val else val
+            nxt_val = d.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
+            res += -val if val < nxt_val else val
         return res
