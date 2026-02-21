a216bb24e9993244b3e3a825a61184c6026f539d
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-21 18:04:44 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_strin):
-    if not isintance(roman_string, str):
+    if not isinstance(roman_string, str):
         return 0
 
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
