cfe9b841e808afb7d59c74384be4233d162a19b6
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-22 18:47:36 +0000

    a

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def_list_division(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         res = 0
