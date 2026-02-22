c0a5f8833dd1da1a6ffbf5f2f04a05e429f3fb98
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-22 14:58:30 +0000

    a

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,4 +13,4 @@ def_list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list.append(div_result)
-    return new_list
+        return new_list
