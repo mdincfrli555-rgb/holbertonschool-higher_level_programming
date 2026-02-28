7e72aafcf92a20a8690e24087306ff348f64779e
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-28 13:38:21 +0000

    a

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines a Square class."""
+"""This module contains a class that defines a square.
+The purpose is to provide a base for geometric operations."""
 
 
 class Square:
-    """A class that represents a square."""
-    pass
-
+    """A class Square that defines a square.
+    This is currently an empty class as per task requirements."""
+    pass:
