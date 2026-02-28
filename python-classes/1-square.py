c438a23b0c0f83319b9c2fcc7057d406ca2f1f75
mdincfrli555-rgb <mdincfrli555@gmail.com>
2026-02-28 14:05:56 +0000

    a

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a"""
+
+class Square:
+    """k"""
+
+    def __init__(self, size):
+        """
+        b.
+
+        Args:
+            size: d (private).
+        """
+        self.__size = size
